20626c1c19c705df32a3519d39bef600dc2d4a0f
David Pratt <david@midnightmechanism.com>
2022-10-11 12:50:54 -0500

    Add authenticate-me project

---
+++ API-Project/backend/app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+const { environment } = require("./config");
+const isProduction = environment === "production";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(cookieParser());
+app.use(express.json());
+
+// only use CORS if in production
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+// Routes from Phase 0:
+const routes = require("./routes");
+
+// ...
+
+app.use(routes); // Connect all the routes
+
+// backend/app.js
+// ...
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// backend/app.js
+// ...
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+module.exports = app;
