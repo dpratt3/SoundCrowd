c50df1f63c92ae3f050b8f9950e03e7950d03819
David Pratt <david@midnightmechanism.com>
2022-10-13 15:09:13 -0500

    Add demo songs

--- backend/db/seeders/20221013174738-demo-song.js
+++ backend/db/seeders/20221013174738-demo-song.js
@@ -6,10 +6,93 @@ module.exports = {
     return queryInterface.bulkInsert(
       "Songs",
       [
+        // album 1
         {
           albumId: 1,
           userId: 1,
           title: "Yesterday",
+          description: " A song about the past",
+          url: "www.yesterday.com",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumId: 1,
+          userId: 2,
+          title: "Tomorrow",
+          description: " A song about the past",
+          url: "www.yesterday.com",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumId: 1,
+          userId: 3,
+          title: "Next Week",
+          description: " A song about the past",
+          url: "www.yesterday.com",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        //album 2
+        {
+          albumId: 2,
+          userId: 1,
+          title: "Red",
+          description: " A song about the past",
+          url: "www.yesterday.com",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumId: 2,
+          userId: 2,
+          title: "Orange",
+          description: " A song about the past",
+          url: "www.yesterday.com",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumId: 2,
+          userId: 3,
+          title: "Yellow",
+          description: " A song about the past",
+          url: "www.yesterday.com",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        // Album 3
+        {
+          albumId: 3,
+          userId: 1,
+          title: "Tick",
+          description: " A song about the past",
+          url: "www.yesterday.com",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumId: 3,
+          userId: 2,
+          title: "Tack",
+          description: " A song about the past",
+          url: "www.yesterday.com",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumId: 3,
+          userId: 3,
+          title: "Toe",
           description: " A song about the past",
           url: "www.yesterday.com",
           imageUrl: "www.google.com",
@@ -26,7 +109,22 @@ module.exports = {
     return queryInterface.bulkDelete(
       "Songs",
       {
-        title: { [Op.in]: ["Yesterday"] },
+        title: {
+          [Op.in]: [
+            // Album 1
+            "Yesterday",
+            "Tomorrow",
+            "Next Week",
+            // Album 2
+            "Red",
+            "Orange",
+            "Blue",
+            // Album 3
+            "Tic",
+            "Tac",
+            "Toe",
+          ],
+        },
       },
       {}
     );
