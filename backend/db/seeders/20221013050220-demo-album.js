c04212aeb6bdb3b8521c5a5aabfe953278bc7e48
David Pratt <david@midnightmechanism.com>
2022-10-13 14:07:50 -0500

    Add three albums for user 1

--- backend/db/seeders/20221013050220-demo-album.js
+++ backend/db/seeders/20221013050220-demo-album.js
@@ -14,6 +14,22 @@ module.exports = {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
+        {
+          userId: 1,
+          title: "Goodbye World",
+          description: "An antitechnology adventure",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 1,
+          title: "Goodnight Moon",
+          description: "An album inspired by the childrens' story",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
       ],
       {}
     );
@@ -24,7 +40,7 @@ module.exports = {
     return queryInterface.bulkDelete(
       "Albums",
       {
-        title: { [Op.in]: ["Hello World"] },
+        title: { [Op.in]: ["Hello World", "Goodbye World", "Goodnight Moon"] },
       },
       {}
     );
