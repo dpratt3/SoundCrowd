2dac6dd8a726077a5190c5021e06f055e5e4cc4e
David Pratt <david@midnightmechanism.com>
2022-10-13 00:11:17 -0500

    Create successful seed data for album

---
+++ backend/db/seeders/20221013050220-demo-album.js
@@ -0,0 +1,32 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Albums",
+      [
+        {
+          userId: 1,
+          title: "Hello World",
+          description: "A song about computers",
+          imageUrl: "www.google.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      "Albums",
+      {
+        title: { [Op.in]: ["Hello World"] },
+      },
+      {}
+    );
+  },
+};
