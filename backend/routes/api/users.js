8a3c8f77e677c3f3d6233ad0f6c853d9e691847b
David Pratt <david@midnightmechanism.com>
2022-10-13 22:47:03 -0500

    Add route for Get All Songs

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -28,7 +28,7 @@
 const express = require("express");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User } = require("../../db/models");
+const { User, Song } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
@@ -61,6 +61,12 @@ router.post("/", validateSignup, async (req, res) => {
   return res.json({
     user,
   });
+});
+
+router.get("/songs", async (req, res) => {
+  const songs = await Song.findAll();
+  console.log(songs);
+  return res.json(songs);
 });
 
 module.exports = router;
