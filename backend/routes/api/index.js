4d23683ee8f05d085b94002e21670be01377c8ce
David Pratt <david@midnightmechanism.com>
2022-10-14 21:12:04 -0500

    Addmissing test router from Authenticate Me Backend Walkthrough 11:23

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,6 +7,10 @@ const { restoreUser } = require("../../utils/auth.js");
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
+router.get("/test", requireAuth, (req, res) => {
+  res.json({ message: "success" });
+});
+
 router.use(restoreUser);
 
 router.use("/session", sessionRouter);
