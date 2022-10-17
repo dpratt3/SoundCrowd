c0b0d9c7a4973eb2f2d97501bac450205467d594
David Pratt <david@midnightmechanism.com>
2022-10-17 15:41:22 -0500

    Begin feature 2,create an album

---
+++ backend/routes/api/albums.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User, Song, Album } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const router = express.Router();
+
+router.post("/", requireAuth, async (req, res) => {
+  const { title, description, imageUrl } = req.body;
+
+  if (!title) {
+    return res.json({
+      message: "Validation error",
+      statusCode: 400,
+      errors: {
+        title: "Album title is required",
+      },
+    });
+  }
+
+  const album = await Album.create({
+    userId: req.user.id,
+    title: title,
+    description: description,
+    imageUrl: imageUrl,
+  });
+
+  return res.json(album);
+});
+
+module.exports = router;
