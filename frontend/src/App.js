b69870def3caeebf68f8de2544aac953d548e52f
David Pratt <david@midnightmechanism.com>
2022-11-19 15:58:46 -0600

    Finish phase 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,46 @@
 // export default App;
 
 // frontend/src/App.js
-import React from "react";
+// import React from "react";
+// import { Route, Switch } from "react-router-dom";
+// import LoginFormPage from "./components/LoginFormPage";
+
+// function App() {
+//   return (
+//     <Switch>
+//       <Route path="/login">
+//         <h1>You're on the login page</h1>
+//         <LoginFormPage />
+//       </Route>
+//     </Switch>
+//   );
+// }
+
+// export default App;
+
+// frontend/src/App.js
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
 
 function App() {
+  <h1>wELCOM</h1>;
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <Switch>
-      <Route path="/login">
-        <h1>You're on the login page</h1>
-        <LoginFormPage />
-      </Route>
-    </Switch>
+    isLoaded && (
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    )
   );
 }
 
