d68449de76edffe1c47162cc6502294688697f62
David Pratt <david@midnightmechanism.com>
2022-11-19 17:23:07 -0600

    Fix login form that disappeared

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,28 +25,18 @@
 // export default App;
 
 // frontend/src/App.js
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+// frontend/src/App.js
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
-import * as sessionActions from "./store/session";
 
 function App() {
-  <h1>wELCOM</h1>;
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
-
   return (
-    isLoaded && (
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-      </Switch>
-    )
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
