5df21ef2ccb657bbc1c6f70b39044c29e4554edc
David Pratt <david@midnightmechanism.com>
2022-11-18 22:07:07 -0600

    Complete frontend setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -51,4 +51,12 @@ router.get("/api/csrf/restore", (req, res) => {
   });
 });
 
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.json({});
+  });
+}
+
 module.exports = router;
