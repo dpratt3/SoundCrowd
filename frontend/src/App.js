f9057cb18991570a236925bef926cfcf4713ada9
David Pratt <david@midnightmechanism.com>
2022-11-19 15:30:48 -0600

    Make login page look better with CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,24 @@
+// function App() {
+//   return (
+//     <h1>Hello from App</h1>
+//   );
+// }
+
+// export default App;
+
+// frontend/src/App.js
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/login">
+        <h1>You're on the login page</h1>
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
