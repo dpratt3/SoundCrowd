e1059b163a66b621db273038727608dfc8ebf4c2
David Pratt <david@midnightmechanism.com>
2022-11-21 15:38:55 -0600

    Update App.js to mach end of phase 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,25 @@
-// function App() {
-//   return (
-//     <h1>Hello from App</h1>
-//   );
-// }
-
-// export default App;
-
 // frontend/src/App.js
-// import React from "react";
-// import { Route, Switch } from "react-router-dom";
-// import LoginFormPage from "./components/LoginFormPage";
-
-// function App() {
-//   return (
-//     <Switch>
-//       <Route path="/login">
-//         <h1>You're on the login page</h1>
-//         <LoginFormPage />
-//       </Route>
-//     </Switch>
-//   );
-// }
-
-// export default App;
-
-// frontend/src/App.js
-// frontend/src/App.js
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
 
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-    </Switch>
+    isLoaded && (
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    )
   );
 }
 
