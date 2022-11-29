94cf09bcbdff0b6dcf9bf0b2207fd09a21ce9d86
David Pratt <david@midnightmechanism.com>
2022-11-28 19:04:59 -0600

    Get authenticate me officially approved

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,51 +1,33 @@
 // frontend/src/components/LoginFormPage/index.js
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-
-import "./LoginForm.css";
 
 function LoginFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
-  const [validationErrors, setValidationErrors] = useState([]);
-  const [hasSubmitted, setHasSubmitted] = useState(false);
-
-  useEffect(() => {
-    const errors = [];
-    if (!credential.length) errors.push("Please enter your username or email.");
-    if (!password.length) errors.push("Please provide a valid password.");
-    setValidationErrors(errors);
-  }, [credential, password]);
+  const [errors, setErrors] = useState([]);
 
   if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    setHasSubmitted(true);
-    if (validationErrors.length) return alert(`Cannot Submit`);
-
-    setValidationErrors([]);
-
-    const formInfo = {
-      credential,
-      password,
-    };
-
-    console.log(formInfo);
-    setCredential("");
-    setPassword("");
-    setHasSubmitted(false);
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <ul>
-        {validationErrors.map((error, idx) => (
+        {errors.map((error, idx) => (
           <li key={idx}>{error}</li>
         ))}
       </ul>
