755332c74fed9e972e09e1ea49d44627b4afa672
David Pratt <david@midnightmechanism.com>
2022-12-02 13:55:34 -0800

    Add delete button as option only if user owns song

--- frontend/src/components/SingleSongPage/index.js
+++ frontend/src/components/SingleSongPage/index.js
@@ -11,22 +11,29 @@ const SongDetail = () => {
     const {songId} = useParams();   
 
     const oneSong = useSelector( (state) => Object.values(state.song)[0] );
+    const sessionUser = useSelector( (state) => state.session.user);
 
     useEffect(() => {
         dispatch(getTheSong(songId));
     }, [dispatch, songId])
 
-    console.log("hello world", songId, ` <--------------------------`)
-
-    return ( 
-            <div> 
-                <NavLink key={oneSong.id} to={`/songs/${oneSong.id}`}>
-                    <div> 
-                        {oneSong.title}, {oneSong.description}
-                    </div>
-                </NavLink>
-                <button>Delete Song</button>
-            </div>
+    //console.log("hello world", songId, ` <--------------------------`)
+    console.log(oneSong.userId, sessionUser.id, ` <----------------------------`)
+    return (
+            <>
+                <div> 
+                    <NavLink key={oneSong.id} to={`/songs/${oneSong.id}`}>
+                        <div> 
+                            {oneSong.title}, {oneSong.description}
+                        </div>
+                    </NavLink>
+                </div>
+                <div className="song-buttons">
+                    {(oneSong.userId === sessionUser?.id) && (
+                    <button onClick={() => console.log("delete me!")}>Delete</button>
+                )}
+                </div>
+            </> 
      );
 }
  
