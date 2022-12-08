4f9929cb7d7c274f27fc8aa98e496b24d0de660a
David Pratt <david@midnightmechanism.com>
2022-12-07 20:24:05 -0800

    Style navbar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -12,31 +12,38 @@ function Navigation({ isLoaded }) {
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <>
-        <ProfileButton user={sessionUser} />
-        <NavLink to="/songs">Songs </NavLink>
-        <NavLink to="/albums">Albums </NavLink>
-        <NavLink to="/my-songs">My-Songs </NavLink>
-      </>
+      <div style={{
+        position: "relative",
+        display: "flex",
+      justifyContent: "space-between", alignItems: 'center', textAlign: "center"}}>
+        <img src="/favicon.ico" style={{width: 40}}></img>
+        <NavLink style={{color: "#fff", textDecoration: "none"}} exact to="/">Home </NavLink>
+        <NavLink style={{color: "#fff", textDecoration: "none"}} to="/songs">Songs </NavLink>
+        <NavLink style={{color: "#fff", textDecoration: "none"}} to="/albums">Albums </NavLink>
+        <NavLink style={{color: "#fff", textDecoration: "none"}} to="/my-songs">My-Songs </NavLink>
+        <div style={{position: "relative"}}>
+        <ProfileButton style={{ position: "absolute", backgroundColor: "#333", color: "#fff", textDecoration: "none", display: "block"}} user={sessionUser} />
+        </div>
+      </div>
     );
   } else {
     sessionLinks = (
-      <>
+      <div style={{display: "flex",
+      justifyContent: "space-between",alignItems: "center", textAlign: "center"}}>
+        <img src="/favicon.ico" style={{width: 40}}></img>
+        <NavLink style={{color: "#fff", textDecoration: "none"}} exact to="/">Home </NavLink>
         <NavLink to="/login">Log In </NavLink>
         <NavLink to="/signup">Sign Up </NavLink>
         <NavLink to="/songs">Songs </NavLink>
         <DemoUserButton />
-      </>
+        </div>
     );
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home </NavLink>
+    <div style={{backgroundColor: "#333",}}>
         {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    </div>
   );
 }
 
