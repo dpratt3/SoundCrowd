2a6bdb4be17def2a9c50b30d6f796e38e12c67a9
David Pratt <david@midnightmechanism.com>
2022-12-07 21:04:30 -0800

    Write inline CSS

--- frontend/src/components/CreateSongForm/index.js
+++ frontend/src/components/CreateSongForm/index.js
@@ -7,7 +7,7 @@ import { createTheSong } from "../../store/songs";
 import { getAllAlbums } from "../../store/album";
 
 
-const CreateSongForm = ({setFormStatus, formStatus}) => {
+const CreateSongForm = ({ setFormStatus, formStatus }) => {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const albumIds = useSelector((state) => Object.values(state.album).filter(album => album.userId == sessionUser.id).map(album => album.title));
@@ -37,71 +37,86 @@ const CreateSongForm = ({setFormStatus, formStatus}) => {
       albumId: Number(albumId)
     };
 
-  
+
     await dispatch(createTheSong(newSong)).catch(
       async (res) => {
         const data = await res.json();
-        if (data && data.errors) {setErrors(data.errors)}
+        if (data && data.errors) { setErrors(data.errors) }
       }
     );
-    if(errors.length == 0){
+    if (errors.length == 0) {
 
       setFormStatus(!formStatus)
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} style={{ maxWidth: 300 }}>
       <ul>
         {errors.map((error, idx) => (
           <li key={idx}>{error}</li>
         ))}
       </ul>
-      <label>
-        Title
+     <div className="form-item">
+        <label>
+          Title
+        </label>
         <input
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           required
         />
-      </label>
-      <label>
-        Description
+      </div>
+
+      <div className="form-item">
+        <label>
+          Description</label>
         <input
           type="text"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           required
         />
-      </label>
-      <label>
-        Url
+      </div>
+
+     <div className="form-item">
+        <label>
+          Url
+        </label>
         <input
           type="text"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
           required
         />
-      </label>
-      <label>
-        ImageUrl
+      </div>
+
+     <div className="form-item">
+        <label>
+          ImageUrl
+        </label>
         <input
           type="text"
           value={imageUrl}
           onChange={(e) => setImageUrl(e.target.value)}
           required
         />
-      </label>
-      <label>
-        AlbumId
-        <select 
-          name="albumId" 
-          onChange={(e) => setAlbumId(e.target.value)} 
+      </div>
+
+     <div className="form-item">
+        <label>
+          AlbumId
+        </label>
+       
+        <select
+          name="albumId"
+          onChange={(e) => setAlbumId(e.target.value)}
           required >
-            {albumIds.map(albumId => <option key={albumId} value={albumId}>{albumId}</option>)}
+          {albumIds.map(albumId => <option key={albumId} value={albumId}>{albumId}</option>)}
         </select>
-      </label>
+        
+      </div>
       <button type="submit">Submit</button>
     </form>
   );
--- frontend/src/components/SongsPage/index.js
+++ frontend/src/components/SongsPage/index.js
@@ -1,7 +1,7 @@
 import "./SongPage.css"
 import { NavLink } from "react-router-dom";
 import { useEffect } from "react";
-import {useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { getAllSongs } from "../../store/songs";
 import { useParams } from "react-router-dom";
 import { getTheSong, deleteTheSong } from "../../store/songs";
@@ -13,7 +13,7 @@ import CreateSongForm from "../CreateSongForm";
 
 const SongPage = () => {
     const dispatch = useDispatch();
-    const allSongs = useSelector( (state) => Object.values(state.song) );
+    const allSongs = useSelector((state) => Object.values(state.song));
     const oneSong = useSelector((state) => Object.values(state.song)[0]);
     const sessionUser = useSelector((state) => state.session.user);
     const { songId } = useParams();
@@ -25,41 +25,46 @@ const SongPage = () => {
         await dispatch(deleteTheSong(songId)).then(() => history.push('/songs'))
         //history.push('/songs')
     };
-    
+
     useEffect(() => {
         dispatch(getAllSongs())
     }, [dispatch])
 
     return (
-        <> 
-        <div>
-            {allSongs && allSongs.length > 0 && allSongs.map((song) => {
-                return(
-                    <div key={song.id}> 
-                        <NavLink to={`/songs/${song.id}`}>
-                            <div> 
-                                {song.title}, {song.description}
-                            </div>
-                        </NavLink>
-                    </div>
-                )
-            })}
-        </div>
-        <div className="song-buttons">
-                {(sessionUser?.id) && (
-                    <button onClick={() => {
-                        setFormStatus(!formStatus)
-                        console.log(formStatus, ` <----------------- form status`)
-                    }
-                    }>Create Song</button>
-                )}
-                {(sessionUser?.id) && (formStatus) && (
-                   <CreateSongForm setFormStatus={setFormStatus} formStatus={formStatus}/>
-                )}
-                
+        <>
+            <div style={{ margin: 20}}>
+                <div className="song-buttons">
+                    {(sessionUser?.id) && (
+                        <button style={{marginBottom: 8}} onClick={() => {
+                            setFormStatus(!formStatus)
+                        }
+                        }>Create Song</button>
+                    )}
+                    {(sessionUser?.id) && (formStatus) && (
+                        <CreateSongForm setFormStatus={setFormStatus} formStatus={formStatus} />
+                    )}
+
+                </div>
             </div>
+            <div style={{ margin: 20, display: "flex", flexWrap: "wrap", justifyContent: "space-between" }}>
+                {allSongs && allSongs.length > 0 && allSongs.map((song) => {
+                    return (
+                        <div key={song.id} onClick={() => history.push(`/songs/${song.id}`)} style={{ flex: "1 0 0", marginTop: 20 }}>
+                            <img src={song.imageUrl} style={{ width: 200, height: 200 }}></img>
+
+                            <div style={{ fontSize: 12, fontWeight: "bold", fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif" }}>
+                                {song.title}
+                            </div>
+                            <div style={{ fontSize: 10, fontWeight: "bold", fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif" }}>
+                                {song.description}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+
         </>
-     );     
+    );
 }
- 
+
 export default SongPage;
